913deb6517b73d7067599b22528455d6fd309dec
KAJI-JAPAN <78065333+KAJI-JAPAN@users.noreply.github.com>
2022-06-24 17:04:57 +0900

    Organism追加

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/UserCard";
 import "./styles.css";
 
 export default function App() {
+  const user = {
+    name: "太郎",
+    image: "https://source.unsplash.com/Mv9hjnEUHR4",
+    email: "sfasfafafa",
+    phone: "sfafasfafafafa",
+    company: {
+      name: "株式会社"
+    }
+  };
   return (
     <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
